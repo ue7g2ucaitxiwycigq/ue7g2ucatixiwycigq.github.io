edd81f1c711b0c24ddf8afd4922505a95765281a
ue7g2ucaitxiwycigq <fycyfycgcyc@gmail.com>
2026-08-03 15:38:05 +0400

    Update telegram.js

--- netlify/functions/telegram.js
+++ netlify/functions/telegram.js
@@ -1,1 +1,34 @@
 
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
+
+  const body = JSON.parse(event.body);
+
+  const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+
+  const response = await fetch(
+    `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: body.chat_id,
+        text: body.text
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+};
